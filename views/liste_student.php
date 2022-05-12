26a3df71467c7e67e3205ba1551e830baf971727
Moundir Jabir <elmondhirjabir@gmail.com>
2022-05-12 21:26:29 +0100

    list

--- views/liste_student.php
+++ views/liste_student.php
@@ -10,13 +10,104 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style/dashboard.css">
+    <link rel="stylesheet" href="style/table.css">
 </head>
 <body>
     <?php require("header.php"); ?>
+
     <main class="main">
-        <h1>liste</h1>
-        <?php var_dump($liste); ?>
+        <div class="Container p-4 ">
+            <div class="d-flex justify-content-between border-bottom fw-bold fs-4">
+                <p class="">Listes des étudiants</p>
+                <button class="btn btn-info"><img style="width: 20px; height: 20px;" src="image/add.png" alt="">
+                    Ajouter
+                </button>
+            </div>
+            <div class="d-flex justify-content-between mt-3 fw-bold">
+                <div class="d-flex">
+                    <p>Afficher</p>
+                    <select class="rounded mx-1" name="" id="">
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="75">75</option>
+                        <option value="tout">tout</option>
+                    </select>
+                    <p>entités</p>
+                </div>
+                <!--  -->
+                <div class="">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="search">
+                            <i class="fa fa-search"></i>
+                            <input type="text" class="form-control" placeholder="Search...">
+                            <button class="btn btn-primary">Search</button>
+                        </div>  
+                    </div>
+                </div>
+                <!--  -->
+
+            </div>
+
+            <div class="table-responsive card mt-3 p-2">
+                <table style="overflow: overlay;" class="table table-striped">
+                    <thead>
+                        <tr class="rounded">
+                            <th scope="col">Name</th>
+                            <th scope="col">Class</th>
+                            <th scope="col">Gender</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="">
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <a href=""><img style="max-width: 20px;" src="/images/trash.png"></a>
+                                <a href="">
+                                    <img style="max-width: 20px;" src="/images/edit.png">
+                                </a>
+                            </td>
+                        </tr>
+                        <tr class="">
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <a href=""><img style="max-width: 20px;" src="/images/trash.png"></a>
+                                <a href="">
+                                    <img style="max-width: 20px;" src="/images/edit.png">
+                                </a>
+                            </td>
+                        </tr>
+                        <tr class="">
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@jfhhefhehfhehfef</td>
+                            <td>
+                                <a href=""><img style="max-width: 20px;" src="/images/trash.png"></a>
+                                <a href="">
+                                    <img style="max-width: 20px;" src="/images/edit.png">
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
     <script src="js/main.js"></script>
 </body>
